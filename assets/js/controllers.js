9920a3335f673c2fcd65732eb940281e7f9e5989
LuisPalaciosUAG <luisgerardo1990@hotmail.com>
2015-08-30 16:55:29 -0500

    Working app web form whose init Cuenta & Perfil

--- assets/js/controllers.js
+++ assets/js/controllers.js
@@ -3,8 +3,6 @@ define(function (require) {
   var angular = require('angular'),
       Controllers = angular.module('controllers', []);
    
-  Controllers.controller('angEmpController', require('controllers/angEmployeeController'));
-
   Controllers.controller('CuentaController', require('controllers/CuentaController'));
 
   Controllers.controller('EditorController', require('controllers/EditorController'));
